30cca6fe0603ed980bb6f4a049cbab7209199f48
Alberto De Bortoli <albertodebortoli@gmail.com>
2023-01-18 18:53:14 +0000

    Reduce visibility of Stellar.FolderConstants

--- Stellar/Sources/Core/Utils/Constants.swift
+++ Stellar/Sources/Core/Utils/Constants.swift
@@ -1,20 +1,21 @@
 //  Constants.swift
 
 struct Constants {
-    public static let stellar = "Stellar"
-    public static let executor = "Executor"
+    static let stellar = "Stellar"
+    static let executor = "Executor"
 }
 
 struct FileConstants {
-    public static let packageDotSwift = "Package.swift"
+    static let packageDotSwift = "Package.swift"
 }
 
 struct FolderConstants {
-    public static let stellarFolder = ".stellar"
-    public static let executablesFolder = "Executables"
-    public static let packagesFolder = "Packages"
-    public static let executorFolder = "Executor"
-    public static let sourcesFolder = "Sources"
+    static let stellarFolder = ".stellar"
+    static let executablesFolder = "Executables"
+    static let packagesFolder = "Packages"
+    static let executorFolder = "Executor"
+    static let sourcesFolder = "Sources"
+    static let templatesFolder = "Templates"
 }
 
 struct TemplateConstants {
