b34447061e36b2a49081a56ea88a7dff85ec9999
Pasquale Cerqua <pasquale.cerqua@assistdigital.comcd>
2023-01-21 19:17:39 +0100

    created Utils directory in Sources/CLI

---
+++ Stellar/Sources/CLI/Utils/FolderConstants.swift
@@ -0,0 +1,8 @@
+//  FolderConstants.swift
+
+import Foundation
+
+struct FolderConstants {
+    static let templatesFolder = "Templates"
+    static let actionsFolder = ".stellar/Actions"
+}
---
+++ Stellar/Sources/CLI/Utils/URLManager.swift
@@ -0,0 +1,24 @@
+//  URLManager.swift
+
+import ArgumentParser
+import Foundation
+
+final class URLManager {
+    
+    func actionsLocation(_ actions: String? = nil) -> URL {
+        if let actions = actions {
+            return URL(fileURLWithPath: actions)
+        }
+        return URL(fileURLWithPath: FileManager.default.currentDirectoryPath)
+            .appendingPathComponent(FolderConstants.actionsFolder)
+    }
+    
+    func templatesLocation(_ templates: String?) -> URL {
+        if let templates = templates {
+            return URL(fileURLWithPath: templates)
+        }
+        return URL(fileURLWithPath: FileManager.default.currentDirectoryPath)
+            .appendingPathComponent(FolderConstants.templatesFolder)
+    }
+    
+}
