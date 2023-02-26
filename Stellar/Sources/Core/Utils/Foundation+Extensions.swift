4bab5495d5b93b1075b29cdcb12c704a9c5c1d29
Daniele Margutti <hello@danielemargutti.com>
2023-02-26 11:45:46 +0100

    #13: Added support for smart file downloads using Swift (instead of cURL)
    This should be useful when mocking data from tests

--- Stellar/Sources/Core/Utils/Foundation+Extensions.swift
+++ Stellar/Sources/Core/Utils/Foundation+Extensions.swift
@@ -72,3 +72,115 @@ extension HTTPURLResponse {
     }
     
 }
+
+// MARK: - URLSession
+
+extension URLSession {
+    
+    /// Buffer size for file download.
+    private static let bufferSize = 65_536
+    
+    /// Download a file synchrously from a given URL and move to the specified directory if necessary.
+    ///
+    /// - Parameters:
+    ///   - URL: URL of the file to download.
+    ///   - destinationURL: destination URL of the file into the local filesystem.
+    ///                     If not specified file will stay in place.
+    /// - Returns: URL of the file.
+    @discardableResult
+    public func downloadFile(atURL url: URL, saveAtURL: URL? = nil) throws -> URL {
+        let fileURL = saveAtURL ?? URL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent(UUID().uuidString)
+
+        let semaphore = DispatchSemaphore(value: 0)
+        let urlRequest = URLRequest(url: url, cachePolicy: .reloadIgnoringCacheData, timeoutInterval: 5)
+        Task {
+            _  = try await download(request: urlRequest, fileURL: fileURL)
+            semaphore.signal()
+        }
+        
+        _ = semaphore.wait(timeout: .distantFuture)
+        return fileURL
+    }
+    
+    /// Download asynchronously a file without loading the entire data in memory
+    /// and save it to specified destination.
+    ///
+    /// - Parameters:
+    ///   - request: request to file.
+    ///   - delegate: optional delegate for session data task.
+    ///   - fileURL: destination local file.
+    /// - Returns: http response
+    public func download(request: URLRequest,
+                         delegate: URLSessionTaskDelegate? = nil,
+                         fileURL: URL) async throws -> URLResponse {
+        
+        let (bytes, httpResponse) = try await bytes(for: request, delegate: delegate)
+        let expectedFileLength = httpResponse.expectedContentLength
+        
+        guard let fileStream = OutputStream(url: fileURL, append: false) else {
+            throw URLError(.cannotOpenFile)
+        }
+        
+        fileStream.open()
+        var bufferedData = Data()
+        bufferedData.reserveCapacity(min(URLSession.bufferSize, Int(expectedFileLength)))
+        
+        // get the data and write it by using the internal buffer to avoid loading the memory.
+        var bytesCount = Int64(0)
+        for try await byte in bytes {
+            try Task.checkCancellation()
+            
+            bytesCount += 1
+            bufferedData.append(byte)
+            
+            if bufferedData.count > URLSession.bufferSize {
+                try fileStream.write(data: bufferedData)
+                bufferedData.removeAll(keepingCapacity: true)
+            }
+        }
+        
+        // some data left on buffer, we'll write it before closing the stream
+        if !bufferedData.isEmpty {
+            try fileStream.write(data: bufferedData)
+        }
+        
+        fileStream.close()
+        
+        return httpResponse
+    }
+    
+}
+
+// MARK: - OutputStream
+
+extension OutputStream {
+    
+    /// Errors writing data.
+    public enum Errors: Error {
+        case bufferFailure
+        case writeFailure
+    }
+    
+    /// Write data into the stream.
+    ///
+    /// - Parameter data: data to write.
+    fileprivate func write(data: Data) throws {
+        try data.withUnsafeBytes { (buffer: UnsafeRawBufferPointer) throws in
+            guard var pointer = buffer.baseAddress?.assumingMemoryBound(to: UInt8.self) else {
+                throw Errors.bufferFailure
+            }
+            
+            var bytesRemaining = buffer.count
+            while bytesRemaining > 0 {
+                let bytesWritten = write(pointer, maxLength: bytesRemaining)
+                if bytesWritten < 0 {
+                    throw Errors.writeFailure
+                }
+                
+                bytesRemaining -= bytesWritten
+                pointer += bytesWritten
+            }
+        }
+    }
+}
+
