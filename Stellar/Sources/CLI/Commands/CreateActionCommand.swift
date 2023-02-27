0ae9f879b9f774b90c63e038c227aafbf5da2831
Alberto De Bortoli <albertodebortoli@gmail.com>
2023-02-27 22:51:10 +0000

    Rename path to outputPath in CreateActionCommand

--- Stellar/Sources/CLI/Commands/CreateActionCommand.swift
+++ Stellar/Sources/CLI/Commands/CreateActionCommand.swift
@@ -13,14 +13,14 @@ struct CreateActionCommand: ParsableCommand {
     private var name: String
     
     @Option(name: .shortAndLong, help: "The path in which to create the action. Optional, defaults to .stellar/Actions")
-    private var path: String?
+    private var outputPath: String?
     
     @Option(name: .shortAndLong, help: "The path to a Templates.bundle. Optional, defaults to the templates shipped with the release.")
     private var templates: String?
     
     func run() throws {
         try validate()
-        let location = URLManager().dotStellarActionsLocation(path)
+        let location = URLManager().dotStellarActionsLocation(outputPath)
         let templatesLocationFactory = TemplatesLocationFactory(templatesPath: templates)
         let actionTemplatesLocation = templatesLocationFactory.actionTemplatesLocation
         let hintTemplatesLocation = templatesLocationFactory.hintTemplatesLocation
