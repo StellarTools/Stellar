b11c571452ad15f6d2269fb0ff02d3f2771ae42e
Daniele Margutti <hello@danielemargutti.com>
2023-03-25 12:52:30 +0100

    Improve event formatting in XCodeLogFormatter

--- Stellar/Sources/Core/Logger/Transports/Event Formatters/XCodeLogEventFormatter.swift
+++ Stellar/Sources/Core/Logger/Transports/Event Formatters/XCodeLogEventFormatter.swift
@@ -1,11 +1,28 @@
-// LogEventFormatters+XCode.swift
+// XCodeLogFormatter.swift
 
 import Foundation
 
-public final class XCodeLogEventFormatter: LogEventMessageFormatter {
-    
+public final class XCodeLogFormatter: LogEventMessageFormatter {
+            
     public func format(event: Log.Event) -> LogSerializableData? {
-        event.message.stringValue
+        switch event.message {
+        case let table as ASCIITable:
+            return "\(event.timestamp.iso8601)\n\(table.stringValue)"
+        default:
+            return "\(event.timestamp.iso8601, pad: .left(25))\(event.level.shortDescription) \(event.message.stringValue)"
+        }
+    }
+    
+}
+
+// MARK: - Date Extension
+
+private extension Date {
+    
+    private static let isoFormatter = ISO8601DateFormatter()
+    
+    var iso8601: String {
+        Date.isoFormatter.string(from: self)
     }
     
 }
